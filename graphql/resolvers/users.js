3717e7777d5ddc125d4a2f2dc12b2655a565fd14
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-27 15:06:13 +0100

    added input validator

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,6 +1,7 @@
 const User = require('../../models/userModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const {validateRegisterInput} = require('../../utils/validators')
 const {UserInputError} = require('apollo-server')
 const tajna = process.env.SECRET || 'ovojemojatajnahahah'
 
@@ -12,7 +13,11 @@ module.exports ={
             { 
             username, password, email, confirmPassword}},
              ){
-
+                const {valid, errors} = validateRegisterInput(username,email, password, confirmPassword)
+                if(!valid){
+                    throw new UserInputError('Input error', {errors})
+                }
+                
                 const user = await User.findOne({username})
                 if(user){
                     throw new UserInputError('User already exists')
