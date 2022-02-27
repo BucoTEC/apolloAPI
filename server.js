e026d9dd803c8dbf85086cd22da0ac68daa72aeb
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-27 12:33:50 +0100

    basic querinig added

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const {ApolloServer} = require('apollo-server')
 const gql  = require('graphql-tag')
 const mongoose = require('mongoose');
+const Post = require('./models/postModel')
 require('dotenv').config()
 
 const db = process.env.MONGO_URL || 'mongodb://localhost:27017/apollo'
 mongoose.connect(db).then(()=>{console.log('connection to db is open');}).catch(err => console.log(err))
 
 const typeDefs = gql`
-
+    type Post{
+        body: String!,
+        username: String!
+    }
     type Query{
-        sayHi : String!
+        allPosts : [Post]
     }
 `
 
 const resolvers = {
     Query:{
-        sayHi: ()=> 'Hello from apollo server'
+        allPosts: async ()=> {
+            try {
+                return await Post.find()
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 }
 
